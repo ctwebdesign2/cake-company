0373231bd32df47cf736d4588117e5432e5e3ee3
ctwebdesign2 <118848923+ctwebdesign2@users.noreply.github.com>
2022-11-23 16:55:13 +0000

    Update javascript.js

--- js/javascript.js
+++ js/javascript.js
@@ -13,7 +13,39 @@ function hamburger() {
     });
 }
 
+/*Checks wheter or not the two emails entered are the same*/
+function validateEmail() {
+    var email = document.getElementById("email");
+    var confirmEmail = document.getElementById("confirmEmail");
+
+    if (email.value != confirmEmail.value) {
+        confirmEmail.setCustomValidity("Emails Don't Match");
+    } else {
+        confirmEmail.setCustomValidity('');
+    }
+
+    email.onchange = validateEmail;
+    confirmEmail.onkeyup = validateEmail;
+}
+
+/*Creates google map*/
+function initMap() {
+    var location = {
+        lat: 53.050440,
+        lng: -7.426794
+    };
+    var map = new google.maps.Map(document.getElementById("map"), {
+        zoom: 13,
+        center: location
+    });
+    var marker = new google.maps.Marker({
+        position: location,
+        map: map
+    });
+}
+
 /*Loads all the javascript on window load*/
 window.onload = () => {
     hamburger();
+    validateEmail();
 };
